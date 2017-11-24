33fb0e89a4482576f11286e5b2bed57f2cb1ddf2
cjgriscom <cjgriscom@gmail.com>
2017-11-24 17:38:54 -0500

    Move control to TkInter

--- sphere.py
+++ sphere.py
@@ -2,8 +2,10 @@
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from OpenGL.GL import *
+from OpenGL.GL.framebufferobjects import *
 import sys
 import math
+import time
 
 import matplotlib.pyplot as plt
 from Tkinter import *
@@ -18,15 +20,40 @@ ALTITUDE =   650000.
 
 def main():
     glutInit(sys.argv)
+    global WINDOWED, SIZE
+    WINDOWED = 0
+    SIZE = 600
+    
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
-    glutInitWindowSize(600, 600)
+    glutInitWindowSize(SIZE, SIZE)
     #glutInitWindowPosition(350, 200)
     glutCreateWindow(name)
+    if not WINDOWED:
+        glutHideWindow()
+        fbo = glGenFramebuffers(1)
+        color_buf = glGenRenderbuffers(1)
+        depth_buf = glGenRenderbuffers(1)
+        glBindFramebuffer(GL_FRAMEBUFFER, fbo)
+        
+        
+        glBindRenderbuffer(GL_RENDERBUFFER, color_buf)
+        glRenderbufferStorage(GL_RENDERBUFFER,GL_RGBA8,SIZE,SIZE)
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, color_buf)
+        
+        glBindRenderbuffer(GL_RENDERBUFFER, depth_buf)
+        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, SIZE,SIZE)
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depth_buf)
+        
+        #glBindFramebuffer(GL_FRAMEBUFFER, 0 )
+        print glCheckFramebufferStatus(GL_FRAMEBUFFER)
+
 
     glClearColor(0., 0., 0., 1.)
     glShadeModel(GL_SMOOTH)
     glEnable(GL_CULL_FACE)
     glEnable(GL_DEPTH_TEST)
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA)
     glEnable(GL_LIGHTING)
     lightZeroPosition = [1.,1.,1., 1.]
     lightZeroColor = [0.8, 1.0, 0.8, 1.0]
@@ -57,7 +84,8 @@ def main():
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
     
     glutDisplayFunc(displayscene) # VCalled once
-    glutSpecialFunc(special) # Called on key
+    glutIdleFunc(displayscene)
+    #glutSpecialFunc(special) # Called on key
     
     glMatrixMode(GL_PROJECTION)
     gluPerspective(fov, 1., ALTITUDE, RAD_EARTH + ALTITUDE) # fovy, aspect, near, far
@@ -66,51 +94,75 @@ def main():
               0, 0, 0,  # Center
               0, 0, RAD_EARTH)  # Up
     glPushMatrix()
-    glutMainLoop()
+    glutPostRedisplay()
+    glutMainLoopEvent()
+    tkMAIN()
     return
 
-# Keyboard controller for sphere
-def special(key, x, y):
-        global earthTex, tex1, tex2
-        global angleA, angleX
-        shift = not (glutGetModifiers() ^ GLUT_ACTIVE_SHIFT)
-        incAmount = 1
-        if shift:
-            incAmount = 7.5
-        if key == GLUT_KEY_F1:
-            if earthTex == tex1:
-                earthTex = tex2
-            else:
-                earthTex = tex1
-        if key == GLUT_KEY_F2:
-            image = getImage(640,640)
-            display(image)
-            
-        if key == GLUT_KEY_UP:
-            angleX += incAmount
-        if key == GLUT_KEY_DOWN:
-            angleX -= incAmount
+def tkMAIN():
+    
+    
+    root=Tk()
+    global canvas, SIZE
+    canvas=Canvas(root, height=SIZE, width=SIZE)
+    
+    repack()
+    canvas.pack(side = TOP, expand=True, fill=BOTH)
+    
+    root.bind('w', handleKeypress)
+    root.bind('a', handleKeypress)
+    root.bind('s', handleKeypress)
+    root.bind('d', handleKeypress)
+    
+    root.mainloop()
 
-        if key == GLUT_KEY_LEFT:
-            angleA += incAmount
-        if key == GLUT_KEY_RIGHT:
-            angleA -= incAmount
+def repack():
+    displayscene()
+    global canvas, SIZE, image, photo #Image and photo must be preserved so GC doesn't eat them
+    image = getImage(SIZE,SIZE)
+    photo = ImageTk.PhotoImage(image)
+    item4 = canvas.create_image(SIZE/2,SIZE/2, image=photo)
+    
 
-        glutPostRedisplay()
+def handleKeypress(event):
+    print event.char
+    key = event.char
+    global earthTex, tex1, tex2
+    global angleA, angleX
+    shift = 0#not (glutGetModifiers() ^ GLUT_ACTIVE_SHIFT)
+    incAmount = 1
+    if shift:
+        incAmount = 7.5
+    if key == 'h':
+        if earthTex == tex1:
+            earthTex = tex2
+        else:
+            earthTex = tex1
+    
+    if key == 'w':
+        angleX += incAmount
+    if key == 's':
+        angleX -= incAmount
+
+    if key == 'a':
+        angleA += incAmount
+    if key == 'd':
+        angleA -= incAmount
+
+    repack()
+        
 
 fov = 141. # 141 is 1.8mm camera lens
 angleX = 0
 angleA = 0
 def displayscene():
-    
+    global WINDOWED
     global sphere, angleA, angleX, fov, earthTex
     global RAD_EARTH
     
+    print 'disp', angleA, angleX
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
     glPushMatrix()
-    
-    
-    
     glRotate(angleA,0,0,1)
     glRotate(angleX,math.sin(angleA/180.*3.1415),math.cos(angleA/180.*3.1415),0)
     glBindTexture(GL_TEXTURE_2D, earthTex)
@@ -118,7 +170,8 @@ def displayscene():
     gluSphere(sphere, RAD_EARTH,    360,     360) 
     
     glPopMatrix()
-    glutSwapBuffers()
+    if WINDOWED: 
+        glutSwapBuffers()
     return
 
 def loadtexture(fileImg):
@@ -148,6 +201,10 @@ def loadtexture(fileImg):
     return textID
 
 def getImage(width, height):
+    global WINDOWED
+    if  not WINDOWED:
+        glPixelStorei(GL_PACK_ALIGNMENT, 1)
+        glReadBuffer(GL_COLOR_ATTACHMENT0)
     buff = glReadPixels(0, 0, width, height, GL_RGB, 
                              GL_UNSIGNED_BYTE)
     image = Image.frombytes(mode="RGB", size=(width, height), 
@@ -155,10 +212,12 @@ def getImage(width, height):
     image = image.transpose(Image.FLIP_TOP_BOTTOM)
     return image
 
-def display(image):
+
+
+def display(image, wWidth, wHeight):
     root=Tk()
-    canvas=Canvas(root, height=640, width=640)
-    basewidth = 640
+    canvas=Canvas(root, height=wHeight, width=wWidth)
+    basewidth = wWidth
     wpercent = (basewidth / float(image.size[0]))
     hsize = int((float(image.size[1]) * float(wpercent)))
     image = image.resize((basewidth, hsize), PIL.Image.ANTIALIAS)

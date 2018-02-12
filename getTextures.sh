6619b18041ee61a50c842d878a6c3cf8682dba4a
Chandler Griscom <cjgriscom@gmail.com>
2018-02-12 15:24:13 -0600

    Clean up getTextures and add ownership note

--- getTextures.sh
+++ getTextures.sh
@@ -1,27 +1,8 @@
-#Jan
-#https://eoimages.gsfc.nasa.gov/images/imagerecords/73000/73938/world.200401.3x21600x10800.png
-#Feb
-#https://eoimages.gsfc.nasa.gov/images/imagerecords/73000/73967/world.200402.3x21600x10800.png
-#Mar
-#https://eoimages.gsfc.nasa.gov/images/imagerecords/73000/73992/world.200403.3x21600x10800.png
-#Apr
-#https://eoimages.gsfc.nasa.gov/images/imagerecords/74000/74017/world.200404.3x21600x10800.png
-#May
-#https://eoimages.gsfc.nasa.gov/images/imagerecords/74000/74042/world.200405.3x21600x10800.png
-#Jun
-#
-#Jul
-#
-#Aug
-#
-#Sep
-#
-#Oct
-#
-#Nov
-#
-#Dec
-#
+# Texture Generation Script for OpenGL Simulator Sources
+# 12 Feb 2018 - Initial Implementation
+
+# Chandler Griscom
+
 dirlist[ 1]=73938
 dirlist[ 2]=73967
 dirlist[ 3]=73992
@@ -37,17 +18,12 @@ dirlist[12]=74218
 
 
 mkdir textures
-##April
-#wget -O textures/tmp.png https://eoimages.gsfc.nasa.gov/images/imagerecords/74000/74017/world.200404.3x21600x10800.png
-#convert -resize 16384x8192 textures/tmp.png april_hi.tga
-#rm textures/tmp.png
 for i in 0{1..9} {10..12};
 do
 echo Downloading month $i
 wget -O textures/tmp.png https://eoimages.gsfc.nasa.gov/images/imagerecords/$(( dirlist[i]-dirlist[i]%1000 ))/$(( dirlist[i] ))/world.2004$i.3x21600x10800.png
-#echo Decompressing month $i - 21600x10800
-#convert textures/tmp.png textures/earth_21600_$i.tga
 echo Decompressing month $i - 16384x8192
-convert textures/tmp.png textures/earth_16384_$i.tga
-rm textures/tmp.png
+convert -resize 16384x8192 textures/tmp.png textures/earth_16384_$i.tga
+rm textures/tmp$i.png
 done
+echo Completed
